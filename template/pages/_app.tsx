2773acf994c07789100280f5f10cb19ef19b82f9
HyeokJinShim <hjshim@mz.co.kr>
2022-11-09 11:01:18 +0900

    testing...

--- template/pages/_app.tsx
+++ template/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+import { RecoilRoot } from "recoil";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+interface MyAppProps extends AppProps {}
+
+const MyApp = (props: MyAppProps) => {
+  const { Component, pageProps } = props;
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>Project</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+};
+
+export default MyApp;
