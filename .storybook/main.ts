095960723f1db8d6f86971f7e9a027d8dba4f736
ShimHyeokJin <shj3497@gmail.com>
2023-10-02 13:24:57 +0900

    storybook 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,33 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type {StorybookConfig} from '@storybook/nextjs';
+import {TsconfigPathsPlugin} from 'tsconfig-paths-webpack-plugin';
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
+  staticDirs: ['../public'],
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  webpackFinal: (config) => {
+    config.resolve ??= {};
+    config.resolve.plugins ??= [];
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    );
+
+    return config;
   },
 };
 export default config;
